53980076a1023e0ee853474f92a187b28e2ae462
Stefano Bisio <stefano.bisio@aia.co.uk>
2020-01-29 21:17:17 +0000

    Introduced react-router-dom with basic hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
     
-    <div style={{
-        backgroundImage: `url(${imageUrl})`,
-    }} 
-    className={`${size} menu-item`}>
+    // On history.push() we could have just put .push(linkUrl). But by composing a string starting with the value of match.props (a page property we can access thanks to withRouter) we can make this component aware of his position in the project, and generate a link starting with the current url it's been displayed on, plus the link we want.
+    <div className={`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}} ></div>
         <div className="content">
-            <div className="title">{title}</div>
+            <div className="title">{title.toUpperCase()}</div>
             <div className="subtitle">Shop now</div>
         </div>
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
