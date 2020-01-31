c696d0db0096553cce103fcee0abd3224887c222
Stefano Bisio <stefano.bisio@aia.co.uk>
2020-01-31 16:24:07 +0000

    Added CollectionItem to the shop page. Added imgs

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './collection-preview.component.scss';
+import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 /* The job of this component is to display the title of a category of clothing, and a few products inside of it (a preview).
 The prop 'title' being one of the 5 categories of products, 'items' being the products under every category. See shop.data.js for reference.
@@ -9,10 +10,10 @@ const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {
-                //Resume from here: need to show less than four per category, as the component is meant to render a preview of the products. 4 max
-                items.map(item => (
-                    <div key={item.id}>{item.name}</div>
+            {items
+                .filter((item, index) => index < 4)
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
