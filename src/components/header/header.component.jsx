340190b18b49261723fa5133187a7b1a6502397b
Stefano Bisio <stefano.bisio@aia.co.uk>
2020-04-28 22:31:57 +0100

    Passed login info to header component
    ...to conditionally render login/out. Added Google styling to login btn

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
 
     <div className="header">
         <Link className="logo-container" to='/'>
@@ -12,7 +13,14 @@ const Header = () => (
         <div className="options">
             <Link className="option" to="/shop">Shop</Link>
             <Link className="option" to="/contact">Contact</Link>
-            <Link className="option" to="/signin">Sign in</Link>
+            
+            {
+                currentUser ?
+                <div className="option" onClick={ () => auth.signOut() }>Sign out</div>
+                :
+                <Link className="option" to="/signin">Sign in</Link>
+            }
+
         </div>
     </div>
 )
